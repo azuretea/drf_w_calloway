c26c5079004bd3c88026c1efa9bcb0c03c94c7a5
Ubuntu <ubuntu@ip-172-31-52-28.ec2.internal>
2015-01-24 21:32:22 +0000

    Django Rest Framework ready

---
+++ resting/resting/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'resting.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^admin/', include(admin.site.urls)),
+    # wiring up rested urls
+    url(r'^', include('rested.urls'))
+)
